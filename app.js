e9349bdfb3e3fe93adc32e46032b4989694542fe
bunmiafolabi <bunmiafolabi98@gmail.com>
2026-02-11 16:35:24 +0000

    feat: add simple HTTP server
    Add basic Node HTTP server that responds with Hello World on port 4000. Uses native http module and logs incoming requests.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const http = require('http');
+import http from "http";
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  console.log(req);
+  res.writeHead(200, {'Content-type': "text/html"});
+  res.end("<h1>Hello World!!</h1>");
 });
 
 server.listen(port, () => {
-  console.log(`port running on port ${port}`)
+  console.log(`port running on port ${port}`);
 })
